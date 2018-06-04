86356072a6650fa954acab4c7290880bc03a84df
krishchandran1994 <32069046+krishchandran1994@users.noreply.github.com>
2018-06-04 07:50:59 -0700

    first

--- src/main/java/com/example/kchan/mappingfitness/constants.java
+++ src/main/java/com/example/kchan/mappingfitness/constants.java
@@ -3,7 +3,9 @@ package com.example.kchan.mappingfitness;
 public class constants {
     public static final String BROADCAST_DETECTED_ACTIVITY = "activity_intent";
 
-    static final long DETECTION_INTERVAL_IN_MILLISECONDS = 30 * 1000;
+    static final long DETECTION_INTERVAL_IN_MILLISECONDS = 25 * 1000;
 
-    public static final int CONFIDENCE = 70;
+    public static final int CONFIDENCE = 80;
+
+
 }
